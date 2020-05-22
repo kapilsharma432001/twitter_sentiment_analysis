37f704e179193c6a46b7d1f388345593fc5bdee4
kapilsharma432001 <sharmakapil432001@gmail.com>
2020-05-22 23:02:19 +0530

    Add files via upload

---
+++ tweepy_streamer.py
@@ -0,0 +1,160 @@
+from tweepy import API
+from tweepy import Cursor
+from tweepy.streaming import StreamListener #This class will allow us to listen tweets
+from tweepy import OAuthHandler #This class will handle authentication based on the credentials that we store in other file
+from textblob import TextBlob
+from tweepy import Stream
+
+import twitter_credentials
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+#### TWITTER CLIENT ####
+
+class TwitterClient():
+    def __init__(self,twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+
+        self.twitter_user = twitter_user
+
+    def get_twitter_client_api(self):
+        return self.twitter_client
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline,id = self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+    def get_friend_list(self,num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends , id = self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+    
+    def get_home_timeline_tweets(self,num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id = self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
+####TWITTER AUNTICATOR ####
+class TwitterAuthenticator():
+
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY , twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN , twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
+        
+        
+
+
+#### TWITTER STREAMER ####
+
+class TwitterStreamer():
+
+    """
+    Class for streaming and receiving live tweets
+    """
+    def __init__(self):
+        self.twitter_authenticator = TwitterAuthenticator()
+    def stream_tweets(self, fetched_tweets_filename , hash_tag_list):
+        # This handles twitter aunthentication and the connection to the Twiiter Streaming API
+        listener = TwitterListener(fetched_tweets_filename)
+        auth = self.twitter_authenticator.authenticate_twitter_app()
+        stream = Stream(auth , listener)
+
+        #The listener object is responsible for how to handle with the data,the tweets and the errors if encountered
+
+        #Now we are going to filter tweets otherwise it will add tons of tweets for which we might not be intersted
+
+        stream.filter(track = hash_tag_list)
+
+        #Now it will take only those tweets which will contain this list of keywords
+
+#### TWITTER STREAM LISTENER ####    
+
+class TwitterListener(StreamListener):
+    """
+    This is a basic listener that will print received tweets to stdout.
+    """
+    def __init__(self,fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+    def on_data(self,data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename,'a') as tf:
+                tf.write(data) # To write the data in file
+            return True
+        except BaseException as e:
+            print("Error on data %s " % str(e))
+        return True
+    def on_error(self,status):
+        #If twitter doesn't like accessing our data...then it will print an error msg and if we continue to access data then Twitter can lock us to access data
+        # So to avoid that we will return false to stop accessing data.
+        if status == 420:
+            return false
+        print(status)
+
+class TweetAnalyzer():
+##    Functionality for analysing and categorizing data from the tweets
+
+
+    def clean_tweet(self,tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+    def analyze_sentiment(self,tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+
+        if analysis.sentiment.polarity>0:
+            return 1
+        elif analysis.sentiment.polarity == 0:
+            return 0
+        else:
+            return -1
+        
+    
+
+        
+    def tweets_to_data_frame(self,tweets):
+        df = pd.DataFrame(data = [tweet.text for tweet in tweets], columns = ['Tweets'])
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+        
+        return df
+
+
+    
+if __name__ == "__main__":
+
+   twitter_client  = TwitterClient()
+   tweet_analyzer = TweetAnalyzer()
+   api = twitter_client.get_twitter_client_api()
+   tweets = api.user_timeline(screen_name = 'kapilsharmak910' , count=2000)
+
+   df = tweet_analyzer.tweets_to_data_frame(tweets)
+
+   df['sentiments'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['Tweets']])
+
+   print(df.head(10))
+   
+
+  
+
+
+   
+   
+   
+    
+
+
+    
+    
+        
